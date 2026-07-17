56b525aa29b2bbaf0254ba97df6d452ba307b96a
ahmed <ahmedmijdo2@gmail.com>
2026-07-17 06:50:49 +0300

    seed schema to database

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client'
+import { faker } from '@faker-js/faker';
+
+const prisma = new PrismaClient();
+async function main () {
+    await prisma.property.createMany({
+        data: Array.from({length: 20}, () => {
+            return {
+                title: faker.internet.username(),
+                description: faker.lorem.text(),
+                type: faker.lorem.text(),
+                address: faker.location.streetAddress(),
+                price: faker.number.float(),
+                ownerName: faker.internet.username(),
+                ownerPhone: faker.phone.number()
+            }
+        })
+    })
+}
+
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
